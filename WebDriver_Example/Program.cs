6f2a036434b8c8bd96179e853b8cf2b4603e8365
Shad Khel <shad.khel@hr-manager.net>
2019-07-17 23:48:33 +0200

    Add project files.

---
+++ WebDriver_Example/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace WebDriver_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //https://www.seleniumhq.org/docs/05_selenium_rc.jsp#programming-your-test
+            //https://www.azuredevopslabs.com/labs/vstsextend/selenium/
+            //https://docs.microsoft.com/en-us/azure/devops/pipelines/test/continuous-test-selenium?view=azure-devops
+
+            IWebDriver driver = new ChromeDriver(AppContext.BaseDirectory)
+            {
+                Url = "https://www.google.com/"
+            };
+
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(0.5);
+
+            var searchbox = driver.FindElement(By.XPath("/html/body/div[1]/div[3]/form/div[2]/div/div[1]/div/div[1]/input"));
+            searchbox.SendKeys("Hello World!" + Keys.Enter);
+            
+            var elements = driver.FindElements(By.TagName("h3"));
+
+            foreach (var e in elements)
+            {
+                Console.WriteLine(e.Text);
+            }
+
+            Console.WriteLine("Results found :" + elements.Count);
+
+            Console.ReadLine();
+        }
+    }
+}
